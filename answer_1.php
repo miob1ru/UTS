27f4f05a93e636b13ffea3835b9bf04e0b767f6e
unknown <putrasumawe56@gmail.com>
2020-04-23 18:37:55 +0700

    answer_1

---
+++ answer_1.php
@@ -0,0 +1,11 @@
+<?php 
+Operator Aritmatika
+$angka = 8;
+ echo $angka + 2; //penambahan
+ echo $angka - 2; // pengurangan
+ echo $angka * 3 + 6; //perkalian 
+ echo $angka + 2; //pertambahan
+
+// pertambahan
+//pertambahan
+?>
